94f7ee423883eebe782a94c193852492e9a13528
Tiago Melo Jucá <tiagomelojuca@gmail.com>
2021-06-27 02:39:02 -0300

    Update dynamic-arrays.c

--- dynamic-arrays.c
+++ dynamic-arrays.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 
+void fill2DArray(double** matrix, int nRows, int nColumns, double initialValue)
+{
+  for(int i = 0; i < nRows; i++) {
+    for(int j = 0; j < nColumns; j++) {
+      matrix[i][j] = initialValue;
+    }
+  }
+}
+
 int main()
 {
+  // 2D Array
   int nRows, nColumns;
   
   double **arr = (double**) malloc(nRows * sizeof(double*));
@@ -9,10 +19,13 @@ int main()
       arr[i] = (double*) malloc(nColumns * sizeof(double));
   }
   
+  fill2DArray(arr, nRows, nColumns, 0.0);
+  
   for(int i = 0; i < nRows; i++) {
       double* currentPtr = arr[i];
       free(currentPtr);
   } free(arr);
   
+  // End
   return 0;
 }
